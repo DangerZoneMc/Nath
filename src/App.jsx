5bcae5b248a8cd89ed4faf6a881bcf8ccf513574
Nathaniel <nieltan8@gmail.com>
2025-02-15 01:38:06 +0800

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import FormExample from "./FormExample";
+import HelloWorld from "./HelloWorld";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <TodoList />
+      <FormExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,6 @@
+function HelloWorld() {
+    return <h1>Hello, React!</h1>;
+  }
+  
+  export default HelloWorld;
+  
