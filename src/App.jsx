af68dc4b717b7a7dfba7ea00f142bedd5229abcb
Nathaniel <nieltan8@gmail.com>
2025-02-18 18:15:43 +0800

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import FormExample from "./FormExample";
-import HelloWorld from "./HelloWorld";
-import TodoList from "./TodoList";
+import React, { useState } from 'react';
+import Calculator from "./components/calculator";
 
 function App() {
   return (
     <div>
-      <HelloWorld />
-      <TodoList />
-      <FormExample />
+      <Calculator />
     </div>
   );
-}
+}const Counter = () => {
+  const [count, setCount] = useState(1);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => {
+    if (count > 1) setCount(count - 1);
+  };
+  const reset = () => setCount(1);
+
+  return (
+    <div className="counter">
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+};
 
 export default App;
